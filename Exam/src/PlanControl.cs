6257136dff6848f0d7f22a5f55e60adb293167ed
ultrasonica9@gmail.com <ultrasonica9@gmail.com>
2024-07-04 10:17:30 +0300

    Add class PlanControl

---
+++ Exam/src/PlanControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace src
+{
+    public class PlanControl
+    {
+        public Subject[] Array { get; private set; }
+        
+        public PlanControl(int size)
+        {
+            Array = new Subject[size];
+        }
+
+        public void Sort()
+        {
+            src.Sort.QuickSort(Array, 0, Array.Length - 1, Comparer<Subject>.Create((x, y) =>
+            {
+                int semesterComparison = x.Semester - y.Semester;
+                if (semesterComparison != 0)
+                {
+                    return semesterComparison;
+                }
+                return string.Compare(x.Name, y.Name);
+            }));
+        }
+
+        public void SaveToFile(string filename)
+        {
+            StreamWriter file = new StreamWriter(filename);
+            foreach(var subj in Array)
+            {
+                file.WriteLine($"Семестр: {subj.Semester}\nНазвание: {subj.Name}\nФамилия преподавателя: {subj.LectorLastname}\n");
+            }
+            file.Flush();
+            file.Dispose();
+        }
+    }
+}
---
+++ Exam/src/Sort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public static class Sort
+    {
+        public static void QuickSort<T>(T[] array, int l, int r, Comparer<T> comparer)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+
+            int pivot = Partion(array, l, r, comparer);
+            QuickSort(array, l, pivot, comparer);
+            QuickSort(array, pivot + 1, r, comparer);
+        }
+
+        private static int Partion<T>(T[] array, int l, int r, Comparer<T> comparer)
+        {
+            T pivot = array[l + (r - l) / 2];
+
+            int i = l;
+            int j = r;
+            while (true)
+            {
+                while(comparer.Compare(array[i], pivot) < 0)
+                {
+                    i++;
+                }
+
+                while(comparer.Compare(array[j], pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i >= j)
+                {
+                    return j;
+                }
+
+                (array[i], array[j]) = (array[j], array[i]);
+                i++;
+                j--;
+            }
+        }
+    }
+}
