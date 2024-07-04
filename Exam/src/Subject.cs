15cf6c9f9fd9734441a4132ea9ca1d34f44fa027
ultrasonica9@gmail.com <ultrasonica9@gmail.com>
2024-07-04 09:54:18 +0300

    Add class Subject

---
+++ Exam/src/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Subject
+    {
+        public string Name { get; set; } = string.Empty;
+        public string LectorLastname { get; set; } = string.Empty;
+        public int Semester { get; set; }
+    }
+}
+ 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Gerasimov_Exam
+Используется .NET Framework, потому что Visual Studio не собирала проекты на .NET Core
